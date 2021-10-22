26396f0a651167d74fcc011781afdae64ce6ac43
nasonkin-aa <game-akaynt@mail.ru>
2021-10-22 17:39:19 +1000

    Fix rotate spider

--- Bat_Run/Assets/Script/Obstacle/Dynamyc/RandomPatrolObstacle.cs
+++ Bat_Run/Assets/Script/Obstacle/Dynamyc/RandomPatrolObstacle.cs
@@ -13,16 +13,21 @@ public class RandomPatrolObstacle : MonoBehaviour
 
     private float _speedObs = 0.5f;
     private bool _stay = true;
-    Vector2 TargetPosition;
 
+    private Rigidbody2D _rb;
+    Vector2 TargetPosition;
+    Vector2 direction;
     void Start()
     {
-        TargetPosition = GetRandomPosition();   
+        TargetPosition = GetRandomPosition();
+        _rb = this.GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
-        //Debug.Log((Vector2)transform.position != TargetPosition);
-        if((Vector2)transform.localPosition != TargetPosition && _stay)
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, 
+            Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.up, direction)), 205 * Time.deltaTime);
+
+        if ((Vector2)transform.localPosition != TargetPosition && _stay)
         {
             transform.localPosition = Vector2.MoveTowards(transform.localPosition, TargetPosition, _speedObs * Time.deltaTime);
         }
@@ -30,14 +35,16 @@ public class RandomPatrolObstacle : MonoBehaviour
         {
             _stay = false;
             StartCoroutine(StopToStart());
-            Debug.Log(TargetPosition);
+           // Debug.Log(TargetPosition);
         }
     }
     private IEnumerator StopToStart()
     {
+
         TargetPosition = GetRandomPosition();
+        RotaionSpider();
         yield return new WaitForSeconds(1f);
-        Debug.Log(TargetPosition);
+       // Debug.Log(TargetPosition);
         _stay = true;
     }
 
@@ -47,4 +54,17 @@ public class RandomPatrolObstacle : MonoBehaviour
         float _randomY = Random.Range(_minY, _maxY);
         return new Vector2(_randomX, _randomY);
     }
+
+    private void RotaionSpider()
+    {
+        direction = new Vector2(
+             (transform.localPosition.x - TargetPosition.x),
+             transform.localPosition.y - TargetPosition.y);
+        
+        direction = -direction ;
+        Debug.Log(Quaternion.Euler(0, 0, Vector2.Angle(direction, Vector2.up)));
+        
+
+    }
 }
+  
